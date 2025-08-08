0eb338f37405036f7b801f9887c50395be295f1d
Ana clara sena dos santos <88paulafsb@gmail.com>
2025-08-08 16:36:11 -0300

    08/08/2024

---
+++ index.py
@@ -0,0 +1,7 @@
+nome = input( 'qual e o seu nome?')
+
+idade= input('qual sua idade ?')
+
+cidade = input ('qual e nome da sua cidade?')
+
+print(f"bem vindo {nome} sua idade é {idade} e o nome da sua cidade {cidade}")
