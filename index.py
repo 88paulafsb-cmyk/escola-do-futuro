31626a411489968b3def25fb97a5cadf8bef0dc2
Ana clara sena dos santos <88paulafsb@gmail.com>
2025-08-12 16:34:09 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
 
 
 
-n1 = float(input("Digite um valor : "))
-n2 = float(input("Digite um valor :  "))
 
 
-if n2 != 0:
-    resultado = n1/ n2
-    print(f"O resultado de {n1} ÷ {n2} é {resultado}")
-else:
-    print("Erro: divisão por zero não é permitida.")
+n1 = float(input("Digite o valor : "))
+n2 = float(input("Digite o valor : "))
+
+
+resultado = n1 * n2
+
+
+print(f"O resultado de {n1} × {n2} é {resultado}")
